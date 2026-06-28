20c6493697e3c4bee99fe63204c908dea86788a5
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-06-28 15:37:49 +0100

    Add Firebase configuration and initialization
    Added Firebase configuration and initialization.

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,1 +1,13 @@
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFzV1hJiY7PnXVf1dzCHCudSBdwaPaKNI",
+  authDomain: "medicare-pro-hms-7c398.firebaseapp.com",
+  projectId: "medicare-pro-hms-7c398",
+  storageBucket: "medicare-pro-hms-7c398.firebasestorage.app",
+  messagingSenderId: "567637459536",
+  appId: "1:567637459536:web:d8c761b15832fc53888755"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
