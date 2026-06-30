87495575856c604dc64930960a527de719d0a89d
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-06-30 16:28:28 +0100

    Set up Firebase configuration and services

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,3 +1,12 @@
+// Firebase SDK Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -7,7 +16,21 @@ const firebaseConfig = {
   storageBucket: "medicare-pro-hms-7c398.firebasestorage.app",
   messagingSenderId: "567637459536",
   appId: "1:567637459536:web:d8c761b15832fc53888755"
+
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
+// Firebase Services
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+const storage = getStorage(app);
+
+
+// Export Services
+export { auth, db, storage };
