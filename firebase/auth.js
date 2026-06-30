9849013ffa80237682cac243ca81e4da52ae26d4
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-06-30 16:07:47 +0100

    Update auth.js

--- firebase/auth.js
+++ firebase/auth.js
@@ -5,3 +5,24 @@ import {
     signOut,
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+export async function loginUser(email, password) {
+
+    try {
+
+        const userCredential =
+            await signInWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+
+        return userCredential.user;
+
+    } catch (error) {
+
+        throw error;
+
+    }
+
+}
