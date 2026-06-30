b5db278dda1f22c4e00ff77e4150e6447c02dc71
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-06-30 16:01:12 +0100

    Import Firebase authentication methods

--- firebase/auth.js
+++ firebase/auth.js
@@ -1,1 +1,7 @@
+import { auth } from "./firebase-config.js";
 
+import {
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
