79f3ece259006ff86bef8ef09eca9d7559236b7a
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-07-01 23:05:44 +0100

    Implement user authentication check in hospitals.js
    Add authentication state listener to redirect unauthenticated users.

--- assets/js/hospitals.js
+++ assets/js/hospitals.js
@@ -1,1 +1,23 @@
+import { auth } from "../../../firebase/firebase-config.js";
 
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+lucide.createIcons();
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+
+        window.location.href = "../../../index.html";
+
+        return;
+
+    }
+
+    console.log("Logged in user:");
+
+    console.log(user);
+
+});
