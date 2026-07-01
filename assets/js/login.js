ef84d289459861ab92cbd8967536c94db19e3993
hamzaibrahi6964-coder <hamzaibrahi6964@gmail.com>
2026-07-01 07:55:11 +0100

    Add login form handling and user authentication
    Implement login functionality with form validation and user authentication.

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,1 +1,48 @@
+import { loginUser } from "../../firebase/auth.js";
 
+lucide.createIcons();
+
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async function (e) {
+
+    e.preventDefault();
+
+    const hospitalDomain =
+        document.getElementById("hospitalDomain").value.trim();
+
+    const email =
+        document.getElementById("email").value.trim();
+
+    const password =
+        document.getElementById("password").value;
+
+    if (
+        hospitalDomain === "" ||
+        email === "" ||
+        password === ""
+    ) {
+
+        alert("Please fill all fields.");
+
+        return;
+
+    }
+
+    try {
+
+        const user = await loginUser(email, password);
+
+        console.log("Logged in:", user);
+
+        alert("Login Successful");
+
+    }
+
+    catch (error) {
+
+        alert(error.message);
+
+    }
+
+});
